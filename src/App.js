6f137b22b1b4204e3341dea11a4027fe18520094
naresh valvi <nvalvi381@gmail.com>
2021-07-23 23:25:16 +0530

    updated Navlink

--- src/App.js
+++ src/App.js
@@ -2,24 +2,22 @@ import './App.css';
 import Home from './components/Home'
 import Search from './components/Search'
 import UserList from './components/UserList';
-import {BrowserRouter as Router,Link,Route,Switch} from 'react-router-dom'
+import {BrowserRouter as Router,NavLink,Route,Switch} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
      <Router>
        <ul>
-        <li>   <Link to="/" className="menu">Home </Link></li>
-       
-      <li> <Link to="/userlist"className="menu" activeClassName="selected">User list</Link> </li>
-      <li> <Link to="/search"className="menu">Search User</Link></li>
+        <li><NavLink to="/" className="menu">Home </NavLink></li>
+       <li><NavLink to="/userlist"className="menu" activeClassName="selected">User list</NavLink> </li>
+      <li> <NavLink to="/search"className="menu">Search User</NavLink></li>
       </ul> 
        <Switch>
          <Route path="/" exact> <Home/> </Route>
          <Route path="/userlist"> <UserList/> </Route>
          <Route path="/search"> <Search/> </Route>
         </Switch>
-       
-     </Router>
+            </Router>
     </div>
   );
 }
